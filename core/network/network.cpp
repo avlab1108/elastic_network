a3ec310ede5125246f6d96320102333452f12914
Minas Hovhannisyan <hovhanminas@mail.ru>
2015-10-22 23:46:40 +0400

    Correcting spacings.

--- core/network/network.cpp
+++ core/network/network.cpp
@@ -1,36 +1,36 @@
 #include "network.h"
 
 point::point(const double x, const double y, const double z) :
-	x(x0),
-	y(y0),
-	z(z0)
+  x(x0),
+  y(y0),
+  z(z0)
 {
 }
 
 network::network(const std::size_t size) :
-	links_(size*(size - 1)/2)
+  links_(size*(size - 1)/2)
 {
-	nodes_.resize(size);
+  nodes_.resize(size);
 }
 
 void network::add_link(const std::size_t node1, const std::size_t node2)
 {
-	const std::size_t max = std::max(node1, node2);
-	const std::size_t min = std::min(node1, node2);
-	links_[min*nodes_.size() - (min-1)*min/2 + max - min].set();
+  const std::size_t max = std::max(node1, node2);
+  const std::size_t min = std::min(node1, node2);
+  links_[min*nodes_.size() - (min-1)*min/2 + max - min].set();
 }
 
 const std::vector<point>& network::nodes() const
 {
-	return nodes_;
+  return nodes_;
 }
 
 const point& network::node_position(const std::size_t node) const
 {
-	return nodes_[node];
+  return nodes_[node];
 }
 
 void network::set_node_position(const std::size_t node, const position& p)
 {
-	nodes_[node] = position;
+  nodes_[node] = position;
 }
--- core/network/network.h
+++ core/network/network.h
@@ -6,10 +6,10 @@
 
 struct point
 {
-	point(const double x0 = 0.0, const double y0 = 0.0, const double z0 = 0.0);
-	double x;
-	double y;
-	double z;
+  point(const double x0 = 0.0, const double y0 = 0.0, const double z0 = 0.0);
+  double x;
+  double y;
+  double z;
 };
 
 typedef point direction;
@@ -17,15 +17,15 @@ typedef point direction;
 class network
 {
 public:
-	network(const std::size_t size);
+  network(const std::size_t size);
 
-	void add_link(const std::size_t node1, const std::size_t node2);
-	const std::vector<point>& nodes() const;
+  void add_link(const std::size_t node1, const std::size_t node2);
+  const std::vector<point>& nodes() const;
 
-	const point& node_position(const std::size_t node) const;
-	void set_node_position(const std::size_t node, const position& p);
+  const point& node_position(const std::size_t node) const;
+  void set_node_position(const std::size_t node, const position& p);
 
 private:
-	std::vector<point> nodes_;
-	boost::dynamic_bitset<> links_;
+  std::vector<point> nodes_;
+  boost::dynamic_bitset<> links_;
 };
