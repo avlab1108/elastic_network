8e3978dbb4e9abe7ccd77fc1e7f2570549f9fda6
Minas Hovhannisyan <hovhanminas@mail.ru>
2015-12-10 20:05:36 +0400

    Moving dynamic library definition to the end of compile command.

--- mkfiles/default_commands.mk
+++ mkfiles/default_commands.mk
@@ -2,6 +2,6 @@ AR_CMD 		= $(RM) $@ ; $(AR) $(ARFLAGS) $@ $^
 
 SLIB_CMD 	= mkdir -p $(OBJ_DIR); $(CPLUSPLUS_SLIB) $(CXX_SPECIFIC_FLAGS) -fPIC -o $@ $(SLIB_ALL) $^ $(SLIB_NONE) $(EXTRA_SLIB_CMD_ARGS)
 
-EXEC_CMD 	= $(CXX) $(CXX_SPECIFIC_FLAGS) $(LDFLAGS) -o $@ $(filter-out %.h, $^)
+EXEC_CMD 	= $(CXX) $(CXX_SPECIFIC_FLAGS) -o $@ $(filter-out %.h, $^) $(LDFLAGS)
 
 DIE 			= ( $(RM) $@; exit 1 )
